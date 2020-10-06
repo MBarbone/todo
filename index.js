b8d8a0c6ef62cd017881bcff62ef77ef1a000cad
Mike <michael.barbone@gmail.com>
2020-10-05 20:42:08 -0400

    user route

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
 
+const usersRoutes = require("./routes/users");
+
 const app = express();
 
 connectDB();
 
 const port = process.env.PORT || 5000;
+
+app.use("api/users", usersRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
---
+++ routes/users.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/Users");
+
+router.post("/", async (req, res) => {
+  console.log("made it 1");
+  try {
+    console.log("made it 2");
+    const { name, email, password } = req.body;
+    console.log(req.body);
+
+    let user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).send("User with that email already exists.");
+    } else {
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      await user.save();
+
+      res.status(201);
+      res.send("User created");
+    }
+  } catch (error) {
+    return res.status(500).send("Server Error");
+  }
+});
+
+module.exports = router;
